9ef80e0fc4160ebfaf1676c1bd10f5925f449f0b
Ben Pig Chu <benpichu@gmail.com>
2017-12-28 08:39:48 +0800

    login

--- client/client.js
+++ client/client.js
@@ -1,10 +1,6 @@
 const net=require("net")
 const readline=require("readline")
 const cmd = readline.createInterface({input:process.stdin})
-cmd.on("line",(line)=>{
-	const command=line.split(" ").filter((str)=>str!=="")
-	console.log(command)
-})
 let inBuffer=Buffer.alloc(0)
 let outBuffer=[]
 let writable=true
@@ -24,12 +20,56 @@ socket.on("drain",()=>{
 		writable=socket.write(outBuffer.shift())
 	}
 })
+let loggedin=false
 socket.on("data",(buf)=>{
 	inBuffer=Buffer.concat([inBuffer,buf])
 	let expectedLength=inBuffer.readInt16BE(0)
 	if(expectedLength+2>=inBuffer.length){
 		let packet=inBuffer.slice(2,expectedLength+2)
-		//process packet
+		inBuffer=inBuffer.slice(expectedLength+2)
+		if(packet.length>0){
+			if(packet[0]===0){
+				if(packet.length>=2){
+					if(packet[1]===0){
+						loggedin=true
+						console.log("Welcome.")
+					}else if(packet[1]===1){
+						console.log("Username do not exist.")
+					}else if(packet[1]===2){
+						console.log("Wrong password.")
+					}else if(packet[1]===3){
+						console.log("Please log out from another client before retry.")
+					}
+				}
+			}
+		}
 	}
-	console.log(buf.toString("utf-8",2))
+})
+cmd.on("line",(line)=>{
+	const command=line.split(" ").filter((str)=>str!=="")
+	if(command.length>0){
+		if(command[0]==="login"){
+			if(command.length!==3){
+				console.log("login <username> <password>")
+			}else{
+				let uBuf=Buffer.from(command[1])
+				if(uBuf.length>16){
+					console.log("username too long")
+					return
+				}
+				let pBuf=Buffer.from(command[2])
+				if(uBuf.length>16){
+					console.log("password too long")
+					return
+				}
+				let data=Buffer.alloc(33)
+				data[0]=0
+				uBuf.copy(data,1)
+				pBuf.copy(data,17)
+				writePacket(data)
+			}
+		}else{
+			console.log("unknown command")
+		}
+	}
 })
--- src/Chat.cpp
+++ src/Chat.cpp
@@ -31,7 +31,7 @@ void chat(){
 	::std::unordered_map<TcpSocket*,Session> socketSessions;
 	EventLoop loop;
 	::std::unique_ptr<TcpServer> server=::std::make_unique<TcpServer>(::std::string("0.0.0.0"),7647);
-	server->setOnConnectHandler([&socketSessions,&loop](TcpSocket* socket){
+	server->setOnConnectHandler([&socketSessions,&loop,&users](TcpSocket* socket){
 		socketSessions[socket];
 		auto sendPacket=[&socketSessions,socket](::std::string data){
 			uint16_t l=data.length();
@@ -44,7 +44,12 @@ void chat(){
 				socketSessions[socket].outputBuffer.push_back(packet);
 			}
 		};
-		socket->setOnDataHandler([&socketSessions,&loop,socket,&sendPacket](::std::string data){
+		socket->setOnDataHandler([&socketSessions,&loop,socket,&sendPacket,&users](::std::string data){
+			if(data.length()==0){
+				socketSessions.erase(socket);
+				loop.deleteLater(socket);
+				return;
+			}
 			socketSessions[socket].inputBuffer+=data;
 			while(socketSessions[socket].inputBuffer.length()>2){
 				uint16_t l=*(uint16_t*)(socketSessions[socket].inputBuffer.c_str());
@@ -52,8 +57,37 @@ void chat(){
 				if(socketSessions[socket].inputBuffer.length()>=2+length){
 					::std::string packet=socketSessions[socket].inputBuffer.substr(2,2+length);
 					socketSessions[socket].inputBuffer=socketSessions[socket].inputBuffer.substr(2+length);
-					{
-						sendPacket(::std::string("iz"));
+					if(packet.length()>0){
+						if(packet[0]==0){//login
+							if(packet.length()>=33){
+								::std::string uRaw=packet.substr(1,17)+::std::string("\0",1);
+								::std::string pRaw=packet.substr(17,33)+::std::string("\0",1);
+								::std::string username=::std::string(uRaw.c_str());
+								::std::string password=::std::string(pRaw.c_str());
+								::std::cerr<<"login "<<username<<" "<<password<<"\n";
+								int i=0;
+								for(;i<users.size();i++){
+									if(users[i].name==username){
+										if(users[i].password==password){
+											for(auto pair:socketSessions){
+												if(pair.second.userId==i+1){
+													sendPacket(::std::string("\0\3",2));
+													goto error;
+												}
+											}
+											socketSessions[socket].userId=i+1;
+											sendPacket(::std::string("\0\0",2));
+										}else{
+											sendPacket(::std::string("\0\x02",2));
+										}
+										error:break;
+									}
+								}
+								if(i==users.size()){
+									sendPacket(::std::string("\0\x01",2));
+								}
+							}
+						}
 					}
 				}else{
 					break;
